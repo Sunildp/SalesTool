0324fa4f4457ae788ec5e537401a56df1b19d376
sunildp <sunildp@192.168.2.19>
2017-06-08 00:08:19 -0700

    initial checkin of sales tool

---
+++ SalesTool/src/com/lara/Manager.java
@@ -0,0 +1,11 @@
+package com.lara;
+
+public class Manager {
+
+	public static void main(String[] args) {
+
+		Test test = new Test();
+		test.wish("Sunil");
+	}
+
+}
---
+++ SalesTool/src/com/lara/Test.java
@@ -0,0 +1,10 @@
+package com.lara;
+
+public class Test {
+
+	public void wish(String name)
+	{
+		System.out.println("Hello "+name+" Gud mrng");
+	}
+	
+}
